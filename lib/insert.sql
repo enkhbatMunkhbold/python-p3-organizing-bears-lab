f8f6b6bd1847375f787a5843ffc9dbaa5f12f5a1
Enkhbat Munkhbold <emunkhbold10@gmail.com>
2024-09-07 13:56:05 -0700

    Complete lab

---
+++ lib/insert.sql
@@ -0,0 +1,8 @@
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Mr. Chocolate', 20, 'male', 'black', 'angry', FALSE);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Rowdy', 10, 'male', 'black', 'peaceful', FALSE);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Tabitha', 6, 'female', 'brown', 'joyful', TRUE);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Sergeant Brown', 10, 'male', 'brown', 'authoritarian', TRUE);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Melissa', 13, 'female', 'brown', 'lazy', TRUE);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Grinch', 2, 'male', 'black', 'emotional', TRUE);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Wendy', 6, 'female', 'black', 'friendly', TRUE);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES (NULL, 13, 'male', 'black', 'hungry', TRUE);
