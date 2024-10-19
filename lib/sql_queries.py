9a5845f5a72ff4da0e41b03ec7d91e8cf4fb33b1
Enkhbat Munkhbold <emunkhbold10@gmail.com>
2024-10-19 13:21:37 -0700

    Current Status

--- lib/sql_queries.py
+++ lib/sql_queries.py
@@ -1,38 +1,38 @@
 select_all_female_bears_return_name_and_age = """
     SELECT
-        name,
-        age
+        bears.name,
+        bears.age
     FROM bears
     WHERE sex='F';
 """
 
 select_all_bears_names_and_orders_in_alphabetical_order = """
     SELECT 
-        name
+        bears.name
     FROM bears
-    ORDER BY name;
+    ORDER BY bears.name ASC;
 """
 
 select_all_bears_names_and_ages_that_are_alive_and_order_youngest_to_oldest = """
     SELECT 
-        name,
-        age
+        bears.name,
+        bears.age
     FROM bears
     WHERE alive=TRUE
-    ORDER BY age ASC;
+    ORDER BY bears.age ASC;
 """
 
 select_oldest_bear_and_returns_name_and_age = """
     SELECT 
-        name,
-        age
+        bears.name,
+        bears.age
     FROM bears
-    ORDER BY age DESC LIMIT 1;
+    ORDER BY bears.age DESC LIMIT 1;
 """
 select_youngest_bear_and_returns_name_and_age = """
     SELECT 
-        name,
-        age
+        bears.name,
+        bears.age
     FROM bears
-    ORDER BY age ASC LIMIT 1;
+    ORDER BY bears.age ASC LIMIT 1;
 """
