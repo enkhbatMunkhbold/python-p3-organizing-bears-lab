2f3a18ccdf18bfabb88cb22239dcee63d8a2c874
Enkhbat Munkhbold <emunkhbold10@gmail.com>
2024-10-19 13:33:07 -0700

    Complete Lab

--- lib/insert.sql
+++ lib/insert.sql
@@ -1,8 +1,8 @@
-INSERT INTO bears VALUES (1,'Mr. Chocolate', 20, 'M', 'black', 'angry', FALSE);
-INSERT INTO bears VALUES (2,'Rowdy', 10, 'M', 'black', 'peaceful', FALSE);
-INSERT INTO bears VALUES (3,'Tabitha', 6, 'F', 'brown', 'joyful', TRUE);
-INSERT INTO bears VALUES (4,'Sergeant Brown', 10, 'M', 'brown', 'authoritarian', TRUE);
-INSERT INTO bears VALUES (5,'Melissa', 13, 'F', 'brown', 'lazy', TRUE);
-INSERT INTO bears VALUES (6,'Grinch', 2, 'M', 'black', 'emotional', TRUE);
-INSERT INTO bears VALUES (7,'Wendy', 6, 'F', 'black', 'friendly', TRUE);
-INSERT INTO bears VALUES (8,NULL, 13, 'M', 'black', 'hungry', TRUE);
+INSERT INTO bears VALUES (1,'Mr. Chocolate', 20, 'M', 'black', 'angry', 0);
+INSERT INTO bears VALUES (2,'Rowdy', 10, 'M', 'black', 'peaceful', 0);
+INSERT INTO bears VALUES (3,'Tabitha', 6, 'F', 'brown', 'joyful', 1);
+INSERT INTO bears VALUES (4,'Sergeant Brown', 10, 'M', 'brown', 'authoritarian', 1);
+INSERT INTO bears VALUES (5,'Melissa', 13, 'F', 'brown', 'lazy', 1);
+INSERT INTO bears VALUES (6,'Grinch', 2, 'M', 'black', 'emotional', 1);
+INSERT INTO bears VALUES (7,'Wendy', 6, 'F', 'black', 'friendly', 1);
+INSERT INTO bears VALUES (8,NULL, 13, 'M', 'black', 'hungry', 1);
